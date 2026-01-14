44d3fb7ba729758dd0ca1d9ee9fac6a3c599e679
Jorge Vargas <joluvargaspa@ittepic.edu.mx>
2026-01-13 18:44:23 -0700

    fix merge

--- routes/iemsRoutes.js
+++ routes/iemsRoutes.js
@@ -11,22 +11,6 @@ const { protect, authorize } = require('../middleware/auth');
 
 const router = express.Router();
 
-<<<<<<< Updated upstream
-// Ruta pública para búsqueda
-router.get('/search', searchIEMS);
-
-// Rutas protegidas
-router.use(protect);
-
-router.route('/')
-  .get(getAllIEMS)
-  .post(authorize('Admin Nacional', 'Admin IES'), createIEMS);
-
-router.route('/:id')
-  .get(getIEMSById)
-  .put(authorize('Admin Nacional', 'Admin IES'), updateIEMS)
-  .delete(authorize('Admin Nacional'), deleteIEMS);
-=======
 // Todas las rutas públicas
 router.get('/search', searchIEMS);
 router.get('/', getAllIEMS);
@@ -34,6 +18,5 @@ router.get('/:id', getIEMSById);
 router.post('/', createIEMS);
 router.put('/:id', updateIEMS);
 router.delete('/:id', deleteIEMS);
->>>>>>> Stashed changes
 
 module.exports = router;
