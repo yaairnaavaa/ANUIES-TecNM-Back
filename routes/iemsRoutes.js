418dddf76c45f9976f6570d16916579a43edbf70
Jorge Vargas <joluvargaspa@ittepic.edu.mx>
2026-01-13 18:41:09 -0700

    free endpoints

--- routes/iemsRoutes.js
+++ routes/iemsRoutes.js
@@ -11,6 +11,7 @@ const { protect, authorize } = require('../middleware/auth');
 
 const router = express.Router();
 
+<<<<<<< Updated upstream
 // Ruta pública para búsqueda
 router.get('/search', searchIEMS);
 
@@ -25,5 +26,14 @@ router.route('/:id')
   .get(getIEMSById)
   .put(authorize('Admin Nacional', 'Admin IES'), updateIEMS)
   .delete(authorize('Admin Nacional'), deleteIEMS);
+=======
+// Todas las rutas públicas
+router.get('/search', searchIEMS);
+router.get('/', getAllIEMS);
+router.get('/:id', getIEMSById);
+router.post('/', createIEMS);
+router.put('/:id', updateIEMS);
+router.delete('/:id', deleteIEMS);
+>>>>>>> Stashed changes
 
 module.exports = router;
