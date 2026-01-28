9b8560c4015796e1e17178e5ec764a53b9a4d7ef
Irving Yair Nava Hernández <iryanavahe@ittepic.edu.mx>
2026-01-28 11:40:18 -0700

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -6,9 +6,9 @@ const IEMS_Repository = require("./src/modules/EscuelasMediaSuperior/iems.reposi
 const IEMS_Service = require("./src/modules/EscuelasMediaSuperior/iems.service.js");
 const IEMS_Controller = require("./src/modules/EscuelasMediaSuperior/iemsController.js");
 
-const IES_Repository = require("./src/modules/EscuelasSuperior/ies.repository.js");
-const IES_Service = require("./src/modules/EscuelasSuperior/ies.service.js");
-const IES_Controller = require("./src/modules/EscuelasSuperior/iesController.js");
+//const IES_Repository = require("./src/modules/EscuelasSuperior/ies.repository.js");
+//const IES_Service = require("./src/modules/EscuelasSuperior/ies.service.js");
+//const IES_Controller = require("./src/modules/EscuelasSuperior/iesController.js");
 
 const CicloService = require("./src/modules/Ciclos/ciclo.service.js");
 const CicloRepository = require("./src/modules/Ciclos/ciclo.repository.js");
@@ -25,12 +25,9 @@ const CarrerasController = require("./src/modules/Carreras/carreras.controller.j
 const NotificacionesService = require("./src/modules/Notificaciones/notificaciones.service.js");
 const NotificacionesController = require("./src/modules/Notificaciones/notificaciones.controller.js");
 
-// const FileHandler = require("./utils/fileHandler.js");
-// exports.fileHandler = new FileHandler("tempFiles");
-
 //repositories
 const IEMS_repository = new IEMS_Repository();
-const IES_repository = new IES_Repository();
+//const IES_repository = new IES_Repository();
 const campaignRepository = new CampaignRepository();
 const cicloRepository = new CicloRepository();
 const userRepository = new UserRepository();
@@ -41,14 +38,14 @@ const IEMS_service = new IEMS_Service(IEMS_repository);
 exports.IEMS_controller = new IEMS_Controller(IEMS_service);
 
 //IES BOOTRSTRAP
-const IES_service = new IES_Service(IES_repository, carrerasRepository);
-exports.IES_controller = new IES_Controller(IES_service);
+//const IES_service = new IES_Service(IES_repository, carrerasRepository);
+//exports.IES_controller = new IES_Controller(IES_service);
 
 //CAMPAINGS BOOTRAP
 const campaignService = new CampaignService(
   campaignRepository,
   cicloRepository,
-  IES_repository,
+  //IES_repository,
   IEMS_repository,
 );
 exports.campaignController = new CampaignController(campaignService);
