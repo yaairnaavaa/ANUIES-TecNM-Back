a4f9a2b84089d9b18780d29b6e2888d3bd948b9b
Irving Yair Nava Hernández <iryanavahe@ittepic.edu.mx>
2026-01-28 11:16:46 -0700

    Uncomment API routes for ies, notifications, and examples

--- server.js
+++ server.js
@@ -59,15 +59,15 @@ app.use("/api/roles", require("./routes/roleRoutes"));
 app.use("/api/permissions", require("./routes/permissionRoutes"));
 app.use("/api/menupermissions", require("./routes/menuPermissionRoutes"));
 app.use("/api/periods", require("./routes/periodRoutes"));
-//app.use("/api/ies", require("./src/modules/EscuelasSuperior/iesRoutes"));
+app.use("/api/ies", require("./src/modules/EscuelasSuperior/iesRoutes"));
 app.use("/api/iems", require("./src/modules/EscuelasMediaSuperior/iemsRoutes"));
 //app.use("/api/campaigns", require("./src/modules/Campaigns/campaignRoutes"));
 //app.use("/api/cycles", require("./src/modules/Ciclos/ciclo.routes"));
 //app.use("/api/prospects", require("./routes/prospectRoutes"));
 //app.use("/api/users", require("./src/modules/Users/user.routes"));
 //app.use("/api/careers", require("./src/modules/Carreras/carreras.routes"));
-//app.use("/api/notifications",require("./src/modules/Notificaciones/notificaciones.routes"),);
-//app.use('/api/examples', require('./routes/exampleRoutes'));
+app.use("/api/notifications",require("./src/modules/Notificaciones/notificaciones.routes"),);
+app.use('/api/examples', require('./routes/exampleRoutes'));
 
 // === REGISTRO GLOBAL DE MODELOS ===
 require("./models/MenuPermission");
