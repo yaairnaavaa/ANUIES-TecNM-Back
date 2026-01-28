5434ba9f6e38f5b05295f3ecd9af60c9992e92d9
Jorge Vargas <joluvargaspa@ittepic.edu.mx>
2026-01-28 16:50:37 -0700

    rutas libres

--- src/modules/EscuelasSuperior/iesRoutes.js
+++ src/modules/EscuelasSuperior/iesRoutes.js
@@ -15,21 +15,21 @@ const router = express.Router();
 
 router
   .route("/")
-  .get(protect, IES_controller.getAllIES)
-  .post(protect, authorize("Admin Nacional"), IES_controller.createIES);
+  .get(IES_controller.getAllIES)
+  .post(IES_controller.createIES);
 
 router
   .route("/:id")
-  .get(protect, IES_controller.getIESById)
-  .patch(protect, IES_controller.updateIES)
-  .delete(protect, IES_controller.deactivateIES);
+  .get(IES_controller.getIESById)
+  .patch(IES_controller.updateIES)
+  .delete(IES_controller.deactivateIES);
 
 //CUSTOM ROUTES
 router
   .route("/:id/carreras")
-  .get(protect, IES_controller.getCarrerasDeIES)
-  .post(protect, IES_controller.agregarCarreraDeIES);
+  .get(IES_controller.getCarrerasDeIES)
+  .post(IES_controller.agregarCarreraDeIES);
 
-router.route("/:id/htmlPage").get(protect, IES_controller.getHTMLpage).post(IES_controller.addHTMLpage);
+router.route("/:id/htmlPage").get(IES_controller.getHTMLpage).post(IES_controller.addHTMLpage);
 
 module.exports = router;
