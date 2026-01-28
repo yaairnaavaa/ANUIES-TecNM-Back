c31cc7352814c69bdae595431fb70652e15301c8
Irving Yair Nava Hernández <iryanavahe@ittepic.edu.mx>
2026-01-28 11:14:58 -0700

    Comment out campaigns API route
    Comment out the campaigns route in server.js.

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.use("/api/menupermissions", require("./routes/menuPermissionRoutes"));
 app.use("/api/periods", require("./routes/periodRoutes"));
 //app.use("/api/ies", require("./src/modules/EscuelasSuperior/iesRoutes"));
 //app.use("/api/iems", require("./src/modules/EscuelasMediaSuperior/iemsRoutes"));
-app.use("/api/campaigns", require("./src/modules/Campaigns/campaignRoutes"));
+//app.use("/api/campaigns", require("./src/modules/Campaigns/campaignRoutes"));
 //app.use("/api/cycles", require("./src/modules/Ciclos/ciclo.routes"));
 //app.use("/api/prospects", require("./routes/prospectRoutes"));
 //app.use("/api/users", require("./src/modules/Users/user.routes"));
