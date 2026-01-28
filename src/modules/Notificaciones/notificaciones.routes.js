30b55706fa70366ee73904f7841c8151f84eacdb
Irving Yair Nava Hernández <iryanavahe@ittepic.edu.mx>
2026-01-28 11:32:24 -0700

    Update notificaciones.routes.js

--- src/modules/Notificaciones/notificaciones.routes.js
+++ src/modules/Notificaciones/notificaciones.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 
-const { notificacionesController } = require("./../../../bootstrap");
+//const { notificacionesController } = require("./../../../bootstrap");
+const { notificacionesController } = require("./bootstrap");
 //const { protect } = require("./../../middleware/auth");
 
 const router = Router();
