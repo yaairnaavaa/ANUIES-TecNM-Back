0d011aefd5df65a9678c318458d7275d6de5decf
rojas <rojas.devnet@gmail.com>
2026-01-29 10:56:18 -0700

    ahora si devuelve html

--- controllers/prospectController.js
+++ controllers/prospectController.js
@@ -2,6 +2,7 @@ const Prospect = require("../models/Prospect");
 const asyncHandler = require("./../src/middleware/asyncHandler");
 const { validateCURP } = require("./../utils/curpValidator");
 const IES = require("./../src/modules/EscuelasSuperior/IES.model");
+const Camp = require("./../src/modules/Campaigns/campaign.model");
 
 // @desc    Registrar nuevo interesado (público)
 // @route   POST /api/prospects/register
@@ -98,9 +99,12 @@ exports.updateProspectProfile = asyncHandler(async (req, res) => {
 
   await prospect.save();
 
-  const IESdelProspecto = prospect.firstChoiceIES;
+  const originCampaign = prospect.originCampaign;
 
-  console.log("la ies es: ", IESdelProspecto);
+  let IESdelProspecto =
+    await Camp.findById(originCampaign).select("ies.iesId -_id");
+
+  IESdelProspecto = IESdelProspecto.ies.iesId;
 
   const IEShtml = await IES.findOne(IESdelProspecto).select(
     "branding.htmlPage -_id",
