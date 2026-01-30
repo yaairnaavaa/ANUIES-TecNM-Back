49f4ee38cac492281e137457b06e2856dc95bb16
Yair Nava <iryanavahe@ittepic.edu.mx>
2026-01-29 21:21:46 -0700

    feat: delete code in cycles

--- src/modules/Ciclos/ciclo.model.js
+++ src/modules/Ciclos/ciclo.model.js
@@ -9,15 +9,8 @@ const CicloEscuelas = new mongoose.Schema(
       type: String,
       required: true,
       trim: true,
-    },
-
-    code: {
-      type: String,
-      required: true,
       unique: true,
-      trim: true,
     },
-
     description: {
       type: String,
       trim: true,
