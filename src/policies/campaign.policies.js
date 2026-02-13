8f25403a7cad381c3cd6ed199da3bf195a486cb6
Jorge Vargas <joluvargaspa@ittepic.edu.mx>
2026-02-13 12:04:06 -0700

    fix show campaigns

--- src/policies/campaign.policies.js
+++ src/policies/campaign.policies.js
@@ -4,8 +4,14 @@ exports.scopeCampaignByUser = (user) => {
   if (user.role.name === "Admin Nacional") return {};
 
   if (["Admin IES", "Operativo IES"].includes(user.role.name)) {
+    // Validar que el usuario tenga IES asignado
+    if (!user.ies) {
+      throw new Error(`Usuario con rol ${user.role.name} debe tener una IES asignada`);
+    }
     return {
-      "ies.iesId": { $eq: user.ies.id },
+      "ies.iesId": { $eq: user.ies._id },
     };
   }
+
+  return {};
 };
